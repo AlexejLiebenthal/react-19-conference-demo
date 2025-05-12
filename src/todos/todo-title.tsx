1d62f61bac2e6303f6270916bb9c79ace5e29e27
Alexej Liebenthal <13908853+AlexejLiebenthal@users.noreply.github.com>
2025-05-12 15:19:06 +0200

    use new metadata syntax for page title

--- src/todos/todo-title.tsx
+++ src/todos/todo-title.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-
 import { useTodoStore } from "./todo-store";
 
 export function TodoTitle() {
@@ -8,15 +6,9 @@ export function TodoTitle() {
   const completedTodos = allNotPendingTodos.filter((t) => t.completed);
   const title = `React 19 - Conference Demo - Todos Completed: ${completedTodos.length.toString()} of ${allNotPendingTodos.length.toString()}`;
 
-  useEffect(() => {
-    document.title = title;
-    return () => {
-      document.title = "React 19 - Conference Demo";
-    };
-  }, [title]);
-
   return (
     <>
+      <title>{title}</title>
       <h2 className="text-center text-2xl font-bold">{title}</h2>
     </>
   );
